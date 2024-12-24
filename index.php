af7f72aafcd6c5977911ee461b3a22859704211e
thaisan-un <89756997+thaisansbc@users.noreply.github.com>
2024-12-24 10:35:16 +0700

    database name

--- index.php
+++ index.php
@@ -40,7 +40,7 @@ define('DEMO', 0);
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-    define('ENVIRONMENT', 'development');
+    define('ENVIRONMENT', 'production');
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
